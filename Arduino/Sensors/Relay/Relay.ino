1565c6b4c4c3f45c30e9f0c34651ab6ce57ea5bb
davidvasicek <david.vasicek77@gmail.com>
2017-07-06 14:07:29 +0200

    Add files via upload

---
+++ Arduino/Sensors/Relay/Relay.ino
@@ -0,0 +1,14 @@
+#define RELAYPIN 2  
+
+void setup() {
+  
+  pinMode(RELAYPIN, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(RELAYPIN, HIGH);   // turn the RELAY on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(RELAYPIN, LOW);    // turn the RELAY off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
